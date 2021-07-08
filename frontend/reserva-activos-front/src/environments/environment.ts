da4f26a963325342fdbf43458d8c3f7d4aa0cfb3
lozgonmi <mlozgon@hotmail.com>
2021-07-08 11:37:10 +0200

    cambio environment con barra

--- frontend/reserva-activos-front/src/environments/environment.ts
+++ frontend/reserva-activos-front/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   host: 'http://localhost:8080/',
-  hostControladores: 'https://reservastic.herokuapp.com/api'
+  hostControladores: 'https://reservastic.herokuapp.com/api/'
 };
 
 /*
